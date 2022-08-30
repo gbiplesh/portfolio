80b2b8b3c6c78622830543c32ac23ae97197f49f
gbiplesh <gbiplesh@gmail.com>
2022-08-30 13:02:18 +0930

    Updates

--- src/nav/navLinks.js
+++
@@ -1,8 +0,0 @@
-export const navLinks = [
-	{ navLinkId: 'Home', scrollToId: 'homeContainer' },
-	{ navLinkId: 'About', scrollToId: 'aboutContainer' },
-	{ navLinkId: 'Projects', scrollToId: 'projectsContainer' },
-	{ navLinkId: 'Skills', scrollToId: 'skillsContainer' },
-	{ navLinkId: 'Experience', scrollToId: 'experienceContainer' },
-	{ navLinkId: 'Interests', scrollToId: 'interestsContainer' },
-];
